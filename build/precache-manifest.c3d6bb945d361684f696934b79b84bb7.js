a88df8e192994c66d3e302920d013aa6560bf2e3
Angel <migg.ro3@gmail.com>
2020-04-17 15:36:46 -0500

    v1 done.

---
+++ build/precache-manifest.c3d6bb945d361684f696934b79b84bb7.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce74fe39e3ff3ed2a41e39ed3ca66688",
+    "url": "/index.html"
+  },
+  {
+    "revision": "554c6680adf5de3992a1",
+    "url": "/static/css/main.0ac71dd4.chunk.css"
+  },
+  {
+    "revision": "847e6ce77bb5c8b90659",
+    "url": "/static/js/2.9e27d30f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9e27d30f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "554c6680adf5de3992a1",
+    "url": "/static/js/main.ac66f9df.chunk.js"
+  },
+  {
+    "revision": "1b458cbf1e0582e68774",
+    "url": "/static/js/runtime-main.b2a5e300.js"
+  },
+  {
+    "revision": "bd010b88b62f233d511816a291bd338f",
+    "url": "/static/media/desktop-footer.bd010b88.svg"
+  },
+  {
+    "revision": "4a73a1887da39c758ec5b61d14ae7ce4",
+    "url": "/static/media/desktop-header.4a73a188.svg"
+  },
+  {
+    "revision": "37673604002bf65d6063c59107045581",
+    "url": "/static/media/mobile-footer.37673604.svg"
+  },
+  {
+    "revision": "f86ce0eaaa187d406106d9fb3943d402",
+    "url": "/static/media/mobile-header.f86ce0ea.svg"
+  }
+]);
--- src/App.js
+++ src/App.js
@@ -3,37 +3,116 @@ import "./App.css";
 
 function Footer() {
   return (
-    <footer className="section-block">
-      <button className="primary-button">Ver Github</button>
-      <button className="primary-button">Enviar correo</button>
+    <footer className="section-block bordered">
+      <div className="button-box">
+        <a
+          href="https://github.com/anromen"
+          className="primary-button"
+          target="_blank"
+        >
+          Ver Github
+        </a>
+        <a
+          href="mailto:migg.ro3@gmail.com"
+          className="primary-button"
+          target="_blank"
+        >
+          Enviar correo
+        </a>
+      </div>
     </footer>
   );
 }
 
-function Project() {
-  const classes = "section-block purple";
+function ProjectAnimalWiki() {
   return (
-    <div class={classes}>
-      <div className="project-image">
-        <img src="img/spotify-stats.png" alt="Imagen del proyecto"></img>
-      </div>
+    <div className="section-block gray">
+      <img
+        src="img/animal-wiki.png"
+        alt="Imagen del proyecto"
+        className="project-image"
+      ></img>
+
       <div className="project-description bordered">
-        <div className="project-title">
-          <h2>Spotify</h2>
-          <div className="space">
-            <div></div>
-            <h2 clas>Stats</h2>
+        <div className="project-content">
+          <div className="project-title">
+            <h2>Animal</h2>
+            <h2 className="low-title">
+              <span className="space"></span>Wiki
+            </h2>
+          </div>
+          <p>
+            Una aplicación que funciona como una fuente de consulta sobre los
+            animales salvajes que habitán nuestro planeta. Realice este proyecto
+            por tres razones: el diseño me impactó, quería conocer realmente lo
+            que era Redux y como se conectaba en una aplicación de React y,
+            finalmente, buscaba implementar un diseño adaptable a diferentes
+            tamaños de pantalla (Responsive).
+          </p>
+          <div className="button-box">
+            <a
+              href="https://github.com/anromen/animal-wiki"
+              className="primary-button"
+              target="_blank"
+            >
+              ver código
+            </a>
+            <a
+              href="https://anromen.github.io/animal-wiki/"
+              className="primary-button no-mobile"
+              target="_blank"
+            >
+              ver aplicación
+            </a>
           </div>
         </div>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent
-          luctus mi mauris, vitae molestie mi eleifend vel. Fusce suscipit enim
-          magna, egestas pretium sem pellentesque imperdiet. Quisque tristique
-          neque dui, quis ultrices enim efficitur sit amet. Mauris dignissim
-          purus nec iaculis efficitur. Curabitur aliquam ultricies elementum.
-        </p>
-        <button className="primary-button">ver código</button>
-        <button className="primary-button no-mobile">ver aplicación</button>
+      </div>
+    </div>
+  );
+}
+
+function ProjectSpotifyStats() {
+  return (
+    <div className="section-block purple">
+      <img
+        src="img/spotify-stats.png"
+        alt="Imagen del proyecto"
+        className="project-image"
+      ></img>
+
+      <div className="project-description bordered">
+        <div className="project-content">
+          <div className="project-title">
+            <h2>Spotify</h2>
+            <h2 className="low-title">
+              <span className="space"></span>Stats
+            </h2>
+          </div>
+          <p>
+            Es una página que mata la curiosidad sobre cuales son esos artistas
+            y canciones que se escuchan con mayor insistencia en Spotify. Con
+            esta aplicación quería aprender sobre el proceso para consumir una
+            API externa. Adicionalmente buscaba tener un acercamiento al diseño
+            de páginas web e implementarlo por mi cuenta, jugando con las
+            formas, las posiciones y orientaciones de los elementos.
+          </p>
+          <div className="button-box">
+            <a
+              href="https://github.com/anromen/spotify-stats"
+              className="primary-button"
+              target="_blank"
+            >
+              ver código
+            </a>
+            <a
+              href="https://anromen.github.io/spotify-stats/"
+              className="primary-button no-mobile"
+              target="_blank"
+            >
+              ver aplicación
+            </a>
+          </div>
+        </div>
       </div>
     </div>
   );
@@ -43,7 +122,7 @@ function Header() {
   return (
     <header className="section-block bordered">
       <div className="icon-box">
-        <a href="/" className="icon">
+        <a href="https://github.com/anromen/" className="icon" target="_blank">
           <img src="img/github.png" alt="github" />
         </a>
       </div>
@@ -53,7 +132,7 @@ function Header() {
           <br />
           Ángel
         </h1>
-        <h3>DESARROLLADOR FRONT-END</h3>
+        <h3>DESARROLLADOR FRONT-END JUNIOR</h3>
       </div>
     </header>
   );
@@ -64,7 +143,8 @@ function App() {
     <div class="content">
       <Header />
       <main>
-        <Project />
+        <ProjectSpotifyStats />
+        <ProjectAnimalWiki />
       </main>
       <Footer />
     </div>
